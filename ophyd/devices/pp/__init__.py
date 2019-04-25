375ac4f44698bd476a6c1b83e2bb8e146ade9da4
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-04-25 12:16:33 +0200

    Added comment explaning the purpose of this class

---
+++ ophyd/devices/pp/__init__.py
@@ -0,0 +1,9 @@
+"""Devices with preprocessed data
+
+The devices here will receive the data and preprocess them so that
+the output is more intuitive to the users.
+
+For the raw device see :mod:`bact2.ophyd.devices.raw`
+"""
+
+__all__ = [ 'bpm']
