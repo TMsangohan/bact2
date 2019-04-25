92546a09d2a9ce200eb1929859e752632c0143ff
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-04-25 12:27:45 +0200

    Code clean up Check of data outstanding

--- examples/bpm_reading.py
+++ examples/bpm_reading.py
@@ -1,31 +1,20 @@
-import sys
-import os
-import numpy as np
-import time
-
 from cycler import cycler
-import bluesky.callbacks.best_effort
 
-# update these directories to where you cloned the packages or install them
-sys.path.append('/home/tmerten/github-repos/')
-# sys.path.append('/net/nfs/srv/MachinePhysics/MachineDevelopment/Mertens/github-repos/')
-
-from bluesky.utils import install_qt_kicker
-import bluesky.callbacks as bc
-from bluesky import RunEngine
 import bluesky.plans as bp
+import bluesky.callbacks as bc
+import bluesky.callbacks.best_effort
+from bluesky import RunEngine
 from bluesky.utils import ProgressBarManager
-
-import databroker
+from bluesky.utils import install_qt_kicker
 
 from ophyd import sim
 
 from bact2.ophyd.utils.preprocessors.CounterSink import CounterSink
 from bact2.ophyd.devices.pp.bpm import BPMStorageRing
+import numpy as np
 
 
 def main():
-	print(time.time())
 	# Repeat the measurement 5 times
 	n_meas = 5
 
@@ -35,8 +24,6 @@ def main():
 	freq = np.linspace(f0, f1, 3)
 
 
-
-	# bpm = BPMWaveformDetector(name = "bpm")
 	bpm = BPMStorageRing(name = "bpm")
 	cs = CounterSink(name = "count_bpm_reads", delay = .0)
 
@@ -54,27 +41,14 @@ def main():
 	bec = bc.best_effort.BestEffortCallback()
 
 	RE = RunEngine({})
-	RE.log.setLevel("INFO")
+	RE.log.setLevel("DEBUG")
 	RE.subscribe(bec)
-	# install_qt_kicker()
+	install_qt_kicker()
+	RE.waiting_hook = ProgressBarManager()
 
-	# RE.waiting_hook = ProgressBarManager()
 
-	# db = databroker.Broker.named('temp')
-    # db = databroker.Broker.named('mongo')
-	# RE.subscribe(db.insert)
+	RE(bp.scan_nd(det, sw_freq * repeat))
 
-	uids = RE(bp.scan_nd(det, sw_freq * repeat))
-	print(uids)
-
-	headers = db[uids]
-	header = headers[0]
-	print(header)
-
-	header = db[-1]
-	df = header.table()
-
-	print(df.columns)
 
 if __name__ == '__main__':
 	main()
