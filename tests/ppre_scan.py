7327e19debc974bdca6aacc0b48f445eae32041b
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2019-04-25 17:45:07 +0200

    Test the pulse picking by resonant excitation pulse
    Initial version

--- tests/ppre_scan.py
+++ tests/ppre_scan.py
@@ -1,47 +1,49 @@
+import matplotlib
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+
 import bluesky.plans as bp
 from bluesky.utils import ProgressBarManager
-from bluesky.callbacks import LivePlot
 from bluesky import RunEngine
 import bluesky.callbacks as bc
 import bluesky.callbacks.best_effort
 from cycler import cycler
-from functools import partial
+
+import bact2
+import bact2.bluesky.hacks.callbacks
+from bact2.bluesky.hacks.callbacks import LivePlot
+from bact2.ophyd.devices.raw.ppre_waveform_generator import PPREWaveformgenerator
+from bact2.ophyd.devices.raw.bbfb import BBQRFeedback
+
 import databroker
-import pickle
 
 # If jupyter is used it would be :func:`bluesky.utils.install_nb_kicker`
 # The call to the :class:`bluesky.RunEngine` has then to be made on the
 # notebook command line
 from bluesky.utils import install_qt_kicker
 
-# In the office experimentation with master clock is not the best to have
-from ophyd import Component as Cpt, Device, EpicsSignalRO, EpicsSignal, sim, PVPositionerPC
-from ophyd.status import DeviceStatus, SubscriptionStatus, Status
 
 # Used to generate a dedicated frequency plan
-import copy
 import numpy as np
-import io
-import time
-import threading
+#import io
+#import time
+#import threading
+import functools
 
 
-class PPREWaveformgeneratorFrequency( PVPositionerPC ):
-    setpoint = Cpt(EpicsSignal,   ":setFrq")
-    readback = Cpt(EpicsSignalRO, ":rdFrq")
-
-class PPREWaveformgenerator( Device ):
-    freq = Cpt(PPREWaveformgeneratorFrequency, 'WFGENC1S10G', egu='Hz', settle_time = 1.0,
-            timeout = 1.0,
-               #setting_parameters = .1
-    )
-
+class PPRELivePlot(LivePlot):
+    """Scale plot data
+    """
+    #: scale factor of y data
+    #: mm/ mA
+    scale_dep = 1
+    def update_caches(self, x, y):
+        # Scale to kHz
+        x = x / 1000.
+        # Scale the current
+        y = y * self.scale_dep
+        return super().update_caches(x,y)
     
-
-class BBQRFeedback( Device ):
-    rms = Cpt(EpicsSignalRO,    "BBQR:X:SRAM:MAXRMSVAL")
-
-
 def re():
     bec = bc.best_effort.BestEffortCallback()
     RE = RunEngine({})
@@ -49,18 +51,44 @@ def re():
     RE.subscribe(bec)
     install_qt_kicker()
     RE.waiting_hook = ProgressBarManager()
+
     db = databroker.Broker.named('temp')
     RE.subscribe(db.insert)
+    return RE, db
 
-    return RE
+def execute(RE, cmd = None, ax = None):
+    
+    # Required as installed matplotlib does not have a 
 
-def run():
+    if cmd is None:
+        cmd = create_cmd()
+        
+    uids = RE(cmd(),
+              PPRELivePlot("ppre_fb_rms", "waveform_freq_readback", ax = ax, marker = '.')
+    )
+    ax.set_xlabel("f [khz]")
+    ax.set_ylabel(" bunch size [mm / mA]")
+    return uids
+    
 
-    f0 = 1030e3
-    f1 = 1030.5e3
-    freq = np.linspace(f0, f1, 6)
-    #f1 = 1080e3
-    #freq = np.linspace(f0, f1, 500)
+def analyse(db, uid):
+    headers = db[uid]
+
+    header = headers[0]
+    print(header)
+    print(header.stream_names)
+    df = header.table()
+    df.to_csv("ppre_scan.csv")
+    print(header.table())
+    print(header.fields())
+    
+def create_cmd():
+
+    f0 = 1040e3
+    # f1 = 1030.5e3
+    # freq = np.linspace(f0, f1, 6)
+    f1 = 1080e3
+    freq = np.linspace(f0, f1, 400 + 1)
     wf = PPREWaveformgenerator(name = "waveform")
     if not wf.connected:
         wf.wait_for_connection()
@@ -71,24 +99,17 @@ def run():
     fb = BBQRFeedback(name = "ppre_fb")
     det = [fb]
 
-
-    cmd = partial(bp.scan_nd, det, sw_freq)
+    cmd = functools.partial(bp.scan_nd, det, sw_freq)
     return cmd
 
-    uids = RE(
-    )
-    print(uids)
-    headers = db[uids]
 
-    header = headers[0]
-    print(header)
-    print(header.stream_names)
-    df = header.table()
-    df.to_csv("ppre_scan.csv")
-    print(header.table())
-    print(header.fields())
-
+def run():
+    RE, db = re()
+    uid = execute(RE)
+    analyse(db, uid)
+    
 if __name__ == '__main__':
     #test()
+    plt.ion()
     run()
 
