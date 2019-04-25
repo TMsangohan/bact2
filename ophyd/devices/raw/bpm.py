70b18d8d063d8d7e5e5de35c1c7bdd02e4cf6995
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-04-25 12:13:06 +0200

    Renamed the module as it contains more than one class Removed storage ring. This is now found in pp
    Renamed classes to be more consistent to expectations

--- ophyd/devices/raw/bpm.py
+++ ophyd/devices/raw/bpm.py
@@ -1,26 +1,35 @@
+"""Beam position monitor data
+
+A higher level of access is provided by
+:mod:`bact2.ophyd.bact2.ophyd.devices.pp.bpm`
+"""
 from ophyd import Component as Cpt, EpicsSignalRO
 from ophyd import Device
 
 from bact2.ophyd.devices.pp.VectorSignalRO import VectorSignalRO
 from bact2.ophyd.devices.utils.EnsureNewValueWhenTriggered import EnsureNewValueWhenTriggered
 
-class BPMDetectorStatistics( Device ):
-    """
+class BPMStatistics( Device ):
+    """Average statistics of the beam position monitor
 
-    Warning:
-       Untested code!
+    All BPM's are read by an IOC. Average data are provided by this IOC. These
+    can be accessed by the signal provided in this class.
     """
     mean_x = Cpt(EpicsSignalRO, ":avgV")
     mean_y = Cpt(EpicsSignalRO, ":avgH")
     rms_x  = Cpt(EpicsSignalRO, ":rmsV")
     rms_y  = Cpt(EpicsSignalRO, ":rmsH")
 
-class BPMWaveformDetector( Device ):
-    """
+class BPMPackedData( Device ):
+    """Common readings of all Beam position monitor data
+
+    This class provides access to the data as received by the IOC.
+    A higher level of access is provided by
+    :class:`bact2.ophyd.bact2.ophyd.devices.pp.bpm.BPMWaveform`
     Ensures that always a new value will be read
     """
     #: packed data containing different values
-    waveform = Cpt(VectorSignalRO , ":bdata")
+    packed_data = Cpt(VectorSignalRO , ":bdata")
     #: counter: the last reading?
     counter  = Cpt(EpicsSignalRO,   ":count")
     #: data ready to take?
@@ -31,13 +40,4 @@ class BPMWaveformDetector( Device ):
         self.new_trigger = EnsureNewValueWhenTriggered(minimum_delay = 10e-3, timeout = 3.0)
 
     def trigger(self):
-        return self.new_trigger.trigger_check(self.waveform)
-
-
-class BPMStorageRing( Device ):
-    stat = Cpt(BPMDetectorStatistics, "BPMZR", name = "stat")
-    waveform = Cpt(BPMWaveformDetector, "MDIZ2T5G", name = "wavefrom")
-
-    def trigger(self, *args, **kwargs):
-        r = self.waveform.trigger(*args, **kwargs)
-        return r
+        return self.new_trigger.trigger_check(self.packed_data)
