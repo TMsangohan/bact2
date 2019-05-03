05e8edea2736fba81f2ec564c92e5868ca53fd4d
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2019-05-03 20:28:02 +0200

    Increased freqeuncy for stress test

--- examples/bpm_reading.py
+++ examples/bpm_reading.py
@@ -9,12 +9,12 @@ from bluesky.utils import install_qt_kicker
 
 from ophyd import sim
 
-import sys 
+import sys
 sys.path.append('/home/tmerten/github-repos/')
 # sys.path.append('/net/nfs/srv/MachinePhysics/MachineDevelopment/Mertens/github-repos/')
 sys.path.append('/home/tmerten/gitlab-repos-hzb/suitcase-elasticsearch/')
 
-from suitcase.elasticsearch import Serializer
+# from suitcase.elasticsearch import Serializer
 
 from bact2.ophyd.utils.preprocessors.CounterSink import CounterSink
 from bact2.ophyd.devices.pp.bpm import BPMStorageRing
@@ -23,41 +23,48 @@ import numpy as np
 
 
 def main():
-	# Repeat the measurement 5 times
-	n_meas = 5
+    # Repeat the measurement 5 times
+    n_meas = 20
 
-	# The frequency range
-	f0 = 10
-	f1 = 14
-	freq = np.linspace(f0, f1, 3)
-
-
-	bpm = BPMStorageRing(name = "bpm")
-	cs = CounterSink(name = "count_bpm_reads", delay = .0)
-
-	cs.inform_set_done = bpm.waveform.new_trigger
-	repeat = cycler(cs, range(n_meas))
-
-	sw_freq = cycler(sim.motor, freq)
+    # The frequency range
+    f0 = 10
+    f1 = 14
+    freq = np.linspace(f0, f1, 3)
 
 
-	if not bpm.connected:
-	    bpm.wait_for_connection()
+    bpm = BPMStorageRing(name = "bpm")
+    cs = CounterSink(name = "count_bpm_reads", delay = .0)
+    cs = sim.motor2
 
-	det = [bpm]
+    # cs.inform_set_done = bpm.waveform.new_trigger
+    repeat = cycler(cs, range(n_meas))
 
-	bec = bc.best_effort.BestEffortCallback()
+    sw_freq = cycler(sim.motor, freq)
 
-	RE = RunEngine({})
-	RE.log.setLevel("DEBUG")
-	RE.subscribe(bec)
-	install_qt_kicker()
-	RE.waiting_hook = ProgressBarManager()
 
-	serializer = Serializer('localhost',9200)
-	RE.subscribe(serializer)
+    if not bpm.connected:
+        bpm.wait_for_connection()
 
-	RE(bp.scan_nd(det, sw_freq * repeat))
+    #print (bpm.trigger())
+    #print (bpm.waveform.ready.read())
+    det = [bpm]
+
+    bec = bc.best_effort.BestEffortCallback()
+
+    RE = RunEngine({})
+    # RE.log.setLevel("INFO")
+    #print(dir(bpm))
+    bpm.waveform.validated_data.setLogger(RE.log)
+    bpm.waveform.measurement_state.setLogger(RE.log)
+
+    RE.subscribe(bec)
+    install_qt_kicker()
+    RE.waiting_hook = ProgressBarManager()
+
+    # serializer = Serializer('localhost',9200)
+    # RE.subscribe(serializer)
+
+    RE(bp.scan_nd(det, sw_freq * repeat))
 
 
 if __name__ == '__main__':
