0073c16b93958bf33f898925d1c517961a853711
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2019-05-07 10:00:10 +0200

    Added offset so that readback works Added comments to explain master clock behaviour

--- ophyd/devices/raw/master_clock.py
+++ ophyd/devices/raw/master_clock.py
@@ -1,19 +1,53 @@
+"""
+
+@author: Pierre Schnizer, Markus Ries
+"""
+
 import numpy as np
-from ophyd import Component as Cpt, Device, PVPositionerPC, PVPositioner
+from ophyd import Component as Cpt, Device
 from ophyd import EpicsSignal, EpicsSignalRO
 
-from ..utils import ReachedSetPoint
+from ..utils.ReachedSetPoint import ReachedSetpoint
 
 
-class MasterClockFrequency(ReachedSetPoint.ReachedSetpoint):
+class ReachedSetpointWithOffsetCorrection( ReachedSetpoint ):
+    """Correct master clock read back for orbit corrections
+    """
+    def _correctReadback(self, val):
+        offset = self.offset.get()
+        val = val - offset
+        return val
+
+
+class MasterClockFrequency( ReachedSetpointWithOffsetCorrection ):
     """Access to frequency of the master clock
+
+
+    The master clock frequency is tightly coupled to the real orbit length.
+    For the ideal undisturbed orbit of BESSY II this corresponds to the
+    frequency of :math:`\approx\,499626\, kHz`
+
+
+    If orbit bumps are added or strong insertion devices are activated, the
+    total orbit length is changed. This change is reflected as feed forward
+    in an offset variable.
+
+    The setpoint is given as a value. Then the offset is added and then set
+    to the hardware clock. Only this value can be read back. Therefore the
+    offset must be deduced from the read back so that the setpoint and
+    readback can match.
+
 
     __init__ requires to set setting_parameters.
 
-    Check :class:`device_utils.ReachedSetpoint` for details
+    Check :class:`device_utils.ReachedSetpoint` for details of the init
+    keywords.
     """
     setpoint = Cpt(EpicsSignal,   'MCLKHX251C:freq')
     readback = Cpt(EpicsSignalRO, 'MCLKHX251C:hwRdFreq')
+
+    #: Offset tries to compensate the lengthing of the machine
+    offset   = Cpt(EpicsSignalRO, 'MCLKHX251C:sum_off')
 
 class MasterClock(Device):
     """BESSY II Master clock
@@ -31,7 +65,8 @@ class MasterClock(Device):
                     egu='kHz', limits = (_ref_freq - _df_max, _ref_freq + _df_max),
                     #settle_time = 5.0
                     #setting_parameters = 1.0
-                    setting_parameters = 0.1
+                    setting_parameters = 0.1,
+                    timeout = 2
     )
     #frequency.done_value = 1
 
