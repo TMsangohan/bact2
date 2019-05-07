8e49a7e6682150b871e564a8f6d29cc12fc5f9d5
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2019-05-07 10:13:11 +0200

    Device status can be different instances and used in parallel. Thus it is now stored on the stack

--- ophyd/devices/utils/signal_with_validation.py
+++ ophyd/devices/utils/signal_with_validation.py
@@ -9,6 +9,10 @@ import functools
 from ophyd.status import DeviceStatus, SubscriptionStatus
 from bact2.ophyd.utils.status.ExpectedValueStatus import ExpectedValueStatus
 
+import logging
+
+logger = logging.getLogger()
+
 class FlickerSignal:
     """Returns data, if no further data is provided during validation time
 
@@ -45,8 +49,7 @@ class FlickerSignal:
         # Time to wait that new data arrives
         self.validation_time = validation_time #s
         self.loop = asyncio.get_event_loop()
-        self.__logger = None
-        self._device_status = None
+        self.__logger = logger
         # Used to find if data was resent
         self._n_triggered = 0
         self.tic()
@@ -77,39 +80,43 @@ class FlickerSignal:
         assert(val >= 0)
         self._validation_time = val
 
-    def check_if_new_reading(self, expected_count):
+    def check_if_new_reading(self, expected_count, book_keeping = None):
         """Just allow for it to arrive!
         """
 
         def log_debug(txt):
             tref = time.time() - self._t0
             txt = "tref {:.2f} ".format(tref) + txt
-            #sys.stderr.write(txt + '\n')
-            #sys.stderr.flush()
+            sys.stderr.write(txt + '\n')
+            sys.stderr.flush()
             if self.__logger:
                 self.__logger.info(txt)
 
-        device_status = self._device_status
+        # device_status = self._device_status
+        device_status = book_keeping.device_status
         assert(device_status is not None)
         now_count =  self._n_triggered
         fmt = "New data arrived?  counts: expected {} found {} "
         log_debug(fmt.format(expected_count, now_count))
         if now_count == expected_count:
             log_debug("No new data arrived: done cnt {} unscribing!".format(now_count))
-            self.signal.clear_sub(self.delay_signal_status)
+            assert(self.signal is not None)
             log_debug("No new data arrived: marking device_status {} id({}) as done !".format(device_status, id(device_status)))
             # print("Unwrapped callbacks {} wrapped callbacks {}".format(
             #    self.signal._unwrapped_callbacks,
             #    self.signal._callbacks
             #    ))
-            device_status._finished()
+            device_status._finished(success = True)
+            cid = book_keeping.cid
+            assert(cid is not None)
+            self.signal.unsubscribe(cid)
             self._device_status = None
             del device_status
 
         else:
             log_debug("New data arrived: Expecting other trigger to mark status as done")
 
-    def delay_signal_status(self, *args, **kwargs):
+    def delay_signal_status(self, *args, book_keeping = None, **kwargs):
         """
         """
         def log_debug(txt):
@@ -119,38 +126,54 @@ class FlickerSignal:
                 self.__logger.info(txt)
 
 
+        assert(book_keeping is not None)
+
         self._n_triggered += 1
         ref_cnt = self._n_triggered
         log_debug("Handling reading {}".format(ref_cnt))
 
+        def check_and_finish():
+            nonlocal ref_cnt, book_keeping
+            self.check_if_new_reading(ref_cnt, book_keeping = book_keeping)
+
         if self.loop.is_running():
             #log_debug("Executing using asyncio loop")
-            def check_and_finish():
-                self.check_if_new_reading(ref_cnt)
-
             self.loop.call_later(self.validation_time, check_and_finish)
 
         else:
             #log_debug("Executing using thread")
             def sleep_and_finish():
                 time.sleep(self.validation_time)
-                self.check_if_new_reading(ref_cnt)
+                check_and_finish()
 
             threading.Thread(target=sleep_and_finish, daemon=True).start()
 
         log_debug("Finished delay signal status")
         return True
 
-    def execute_validation(self, device_status):
-        if self._device_status is None:
-            pass
-        else:
-            assert(self._device_status is device_status)
+    def execute_validation(self, device_status, run = True):
+        """
 
-        self._device_status = device_status
+        Warning:
+            Don"t forget to unsubscribe again!
+        """
+        assert(device_status is not None)
+
         self._n_triggered = 0
-        cid = self.signal.subscribe(self.delay_signal_status,  run = True)
-        #print("Subscribers {}".format(self.signal._unwrapped_callbacks))
+
+        class callback:
+            def __init__(self, parent = None, device_status = None):
+                self.parent = parent
+                self.device_status = device_status
+                self.cid = None
+            def __call__(self, *args, **kwargs):
+                self.parent.delay_signal_status(*args, book_keeping = self, **kwargs)
+
+        cb = callback(parent = self, device_status = device_status)
+        cid = self.signal.subscribe(cb,  run = run)
+        cb.cid = cid
+
+        # print("Subscribers {}".format(self.signal._unwrapped_callbacks))
 
     def trigger_and_validate(self):
         #print("Got trigger: state machine state {}".format(self._acquisition_state.state))
@@ -158,8 +181,9 @@ class FlickerSignal:
         if self._timeout is not None:
             kws["timeout"] = self._timeout
 
+        assert(self.signal is not None)
         device_status = DeviceStatus(device=self.signal, timeout = self.timeout)
-        self.execute_validation(device_status)
+        self.execute_validation(device_status, run = False)
         return device_status
 
     def data_read(self):
