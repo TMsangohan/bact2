0af06166bc91612ac069b512bf0a07ed693eebed
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2019-05-08 18:38:37 +0200

    Testing steerer Not yet working

--- examples/steerer_step.py
+++ examples/steerer_step.py
@@ -1,60 +1,116 @@
 import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
-from cycler import cycler
 
 import bluesky.plans as bp
-import bluesky.plan_stubs as bps
 from bluesky.utils import ProgressBarManager
 from bluesky.utils import install_qt_kicker
 
 from bluesky import RunEngine
 import bluesky.callbacks as bc
 import bluesky.callbacks.best_effort
+from ophyd import sim
 
+from cycler import cycler
 
 import bact2
 import bact2.bluesky.hacks.callbacks
 from bact2.bluesky.hacks.callbacks import LivePlot, AxisWrapper
 
-from bact2.ophyd.devices.raw import steerers
+from bact2.ophyd.devices.raw import steerers, bpm
+from bact2.ophyd.utils.preprocessors.CounterSink import CounterSink
 
-import bact2
-import bact2.bluesky.hacks.callbacks
-from bact2.bluesky.hacks.callbacks import LivePlot
+from bact2.ophyd.devices.pp.bpm import BPMStorageRing
+
+
+
+import numpy as np
+
+class OffsetPlot( LivePlot ):
+    def update_caches(self, x, y):
+        """Just show the change
+        """
+        # Scale the current
+        offset = -0.1281
+
+        dx = x - offset
+        dy = y - offset
+
+        t_max = 3
+        scale =1e3
+        sdx = dx / t_max * scale
+        sdy = dy / t_max * scale
+
+        return super().update_caches(sdx, sdy)
+
 
 def main():
     # Repeat the measurement 5 times
 
+    n_meas = 5
+    cs = CounterSink(name = "count_bpm_reads", delay = .2)
+    repeat = cycler(cs, range(n_meas))
+
+
     col = steerers.SteererCollection(name=  "cs")
-    steerer = col.steerers.hs4p1d6r
+    steerer = col.steerers.vs2p1d1r
+
     print(steerer.name)
     det = [steerer]
 
     bec = bc.best_effort.BestEffortCallback()
 
     RE = RunEngine({})
-    #RE.log.setLevel("DEBUG")
+    RE.log.setLevel("DEBUG")
     RE.log.setLevel("INFO")
+    col.setLogger(RE.log)
 
-    currents = (0, 1, -1, 0)
-    steerer_steps = cycler(steerer, currents)
 
 
     RE.subscribe(bec)
     install_qt_kicker()
     RE.waiting_hook = ProgressBarManager()
 
-    def step_steerer():
-        nonlocal currents
-        for t_current in currents:
-            print("Current to run over ")
-            # yield from bps.mv(steerer.dev, t_current)
-            yield from bp.count([steerer.dev.readback])
 
-    RE(step_steerer(),
-           LivePlot("a_steerer_dev_readback", "a_steerer_dev_setpoint",   ax = None, marker = '.'),
-           )
+
+    col.set( 'hs1pd1r')
+    bpm = BPMStorageRing(name = "bpm")
+    if not bpm.connected:
+        bpm.wait_for_connection()
+
+    # dev = col.selected_steerer.dev
+    dev = col.steerers.hs1pd1r.dev
+    det = [
+        bpm,
+        dev.readback,
+        dev.readback,
+        col.sel_readback,
+        col.sel_setpoint,
+    ]
+
+    current_offset = dev.readback.get()
+
+    current_vals = np.array([0, 1, -1, 0])
+    currents = current_vals * 1e-3 + current_offset
+    steerer_steps = cycler(dev, currents)
+
+    f = plt.figure(1, [8, 6])
+    ax1 = plt.subplot(111)
+    ax1 = AxisWrapper(ax1)
+
+
+    RE(bp.scan_nd(det, steerer_steps * repeat),
+       [OffsetPlot(
+           #"cs_sel_readback", "cs_sel_setpoint",
+           'cs_steerers_hs1pd1r_dev_readback',
+           'cs_steerers_hs1pd1r_dev_setpoint',
+           ax = ax1,
+           marker = '.')]
+       )
+    # RE(bp.scan(det, dev, -10e-3 + current_offset, 10e-3 + current_offset, 2),[
 
 if __name__ == '__main__':
+    plt.ion()
     main()
+    plt.ioff()
+    plt.show()
